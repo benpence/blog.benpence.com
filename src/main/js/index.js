92a96749f998769bd5b14144e4fe4ace8e169751
Ben Pence <github@benpence.com>
2016-08-21 12:20:50 -0700

    Replaces synchronous posts fetch with async APIClient iface & impl that uses JQuery

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,55 +1,16 @@
 import React                  from 'react'
 import ReactDOM               from 'react-dom'
-import { Post }               from './model'
-import { User }               from './model'
+import { RemoteApiClient }    from './api'
 import { MostRecentView }     from './view'
 
-const getJson = (url) => {
-  var xmlHttp = new XMLHttpRequest()
-  xmlHttp.open("GET", url, false)
-  xmlHttp.send(null)
-  return JSON.parse(xmlHttp.responseText)
-}
+const client = new RemoteApiClient("/api")
 
-const getNMostRecentPosts = (n) => {
-  const postsJson = getJson("/api/post/most_recent?page=0&page_size=" + n.toString())
-
-  if (postsJson.hasOwnProperty("errors")) {
-    throw "API Errors: " + postsJson.errors.map ( s => s.toString() ).toString()
-
-  } else if (!postsJson.hasOwnProperty("results")) {
-    throw "Unexpected API response: " + postsJson.toString()
-
-  } else {
-    // Example:
-    //
-    // { "results": [
-    //   {
-    //     "id": 0,
-    //     "author":{
-    //       "id": 0,
-    //       "name": "Ben Pence"
-    //     },
-    //     "title": "Spring Reborn!",
-    //     "created_millis": 1471202213000,
-    //     "tags":["spring", "reborn"],
-    //     "content": "<p></p>",
-    //   },
-    //   ...
-    // ]}
-    const { results } = postsJson
-    return results.map( postJson => new Post(
-      postJson.id,
-      new User(postJson.author.id, postJson.author.name),
-      postJson.title,
-      new Date(postJson.created_millis),
-      postJson.tags,
-      postJson.content
-    ))
-  }
-}
-
-ReactDOM.render(
-  <MostRecentView posts = {getNMostRecentPosts(10)} />,
-  document.getElementById('app')
-)
+client
+  .getMostRecentPosts(10)
+  .then( posts => 
+    ReactDOM.render(
+      <MostRecentView posts = {posts} />,
+      document.getElementById('app')
+    )
+  )
+  .catch( t => { throw t })
