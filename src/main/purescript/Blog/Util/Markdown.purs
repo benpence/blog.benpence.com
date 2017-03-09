573e2b2fb329d2d6b8954ce8e38bd3859d2851c3
Ben Pence <git@benpence.com>
2017-03-09 08:37:00 -0800

    Collapses markdown text objects that are the same.
    Unfortunately, after testing, it seems like the parsing is the bottleneck, not rendering

--- src/main/purescript/Blog/Util/Markdown.purs
+++ src/main/purescript/Blog/Util/Markdown.purs
@@ -25,36 +25,40 @@ toPux :: String -> Either String (Html Action)
 toPux markdown =
   let
     blocksToPux :: S.SlamDownP String -> Html Action
-    blocksToPux = H.div [] <<< map blockToPux <<< toArray <<< (\(S.SlamDown bs) -> bs)
+    blocksToPux = H.div [] <<< toArray <<< map blockToPux <<< (\(S.SlamDown bs) -> bs)
   in
     --map blocksToPux parsed
     map blocksToPux (Parser.parseMd markdown)
 
 blockToPux :: S.Block String -> Html Action
-blockToPux S.Rule = H.hr [] []
-blockToPux (S.LinkReference label url) = H.a [A.href url] [H.text label]
--- Evaluating blocks unsupported
-blockToPux (S.CodeBlock _ lines) =
-  let
-    code =
-        if (List.length lines) == 1
-        then map H.text (toArray lines)
-        else [H.text (foldl (\a b -> a <> "\n" <> b) "" lines)]
-  in
-    H.pre [A.className "codeblock"] code
-blockToPux (S.Lst (S.Bullet  _) items) =
-    H.ul [] (map listItemToPux (toArray (map toArray items)))
-blockToPux (S.Lst (S.Ordered _) items) =
-    H.ol [] (map listItemToPux (toArray (map toArray items)))
-blockToPux (S.Blockquote blocks) =
-    H.div [A.className "blockquote"] (map blockToPux (toArray blocks))
-blockToPux (S.Header 1 inlines) = H.h1 [] (map inlineToPux (toArray inlines))
-blockToPux (S.Header 2 inlines) = H.h2 [] (map inlineToPux (toArray inlines))
-blockToPux (S.Header 3 inlines) = H.h3 [] (map inlineToPux (toArray inlines))
-blockToPux (S.Header 4 inlines) = H.h4 [] (map inlineToPux (toArray inlines))
-blockToPux (S.Header 5 inlines) = H.h5 [] (map inlineToPux (toArray inlines))
-blockToPux (S.Header _ inlines) = H.h6 [] (map inlineToPux (toArray inlines))
-blockToPux (S.Paragraph inlines) = H.p [] (map inlineToPux (toArray inlines))
+blockToPux block = case block of
+    S.Rule -> H.hr [] []
+    (S.LinkReference label url) -> H.a [A.href url] [H.text label]
+    -- Evaluating blocks unsupported
+    (S.CodeBlock _ lines) ->
+      let
+        code =
+            if (List.length lines) == 1
+            then toArray (map H.text lines)
+            else [H.text (foldl (\a b -> a <> "\n" <> b) "" lines)]
+      in
+        H.pre [A.className "codeblock"] code
+    (S.Lst (S.Bullet  _) items) ->
+       H.ul [] (map listItemToPux (toArray (map toArray items)))
+    (S.Lst (S.Ordered _) items) ->
+       H.ol [] (map listItemToPux (toArray (map toArray items)))
+    (S.Blockquote blocks) ->
+       H.div [A.className "blockquote"] (toArray (map blockToPux  blocks))
+    (S.Header 1 inlines) -> H.h1 [] (inlinesToPux inlines)
+    (S.Header 2 inlines) -> H.h2 [] (inlinesToPux inlines)
+    (S.Header 3 inlines) -> H.h3 [] (inlinesToPux inlines)
+    (S.Header 4 inlines) -> H.h4 [] (inlinesToPux inlines)
+    (S.Header 5 inlines) -> H.h5 [] (inlinesToPux inlines)
+    (S.Header _ inlines) -> H.h6 [] (inlinesToPux inlines)
+    (S.Paragraph inlines) -> H.p [] (inlinesToPux inlines)
+  where
+    inlinesToPux :: List (S.Inline String) -> Array (Html Action)
+    inlinesToPux = toArray <<< map inlineToPux <<< collapseText
 
 listItemToPux :: Array (S.Block String) -> Html Action
 listItemToPux blocks = H.li [] (map blockToPux blocks)
@@ -81,7 +85,19 @@ inlineToPux inline = case inline of
     (S.Str text) -> H.text text
   where
     inlinesToPux :: List (S.Inline String) -> Array (Html Action)
-    inlinesToPux = map inlineToPux <<< toArray
+    inlinesToPux = toArray <<< map inlineToPux <<< collapseText
+
+collapseText :: List (S.Inline String) -> List (S.Inline String)
+collapseText =
+  let
+    combine :: List (S.Inline String) -> S.Inline String -> List (S.Inline String)
+    combine (List.Cons (S.Str acc) tail) (S.Str e) =
+        List.Cons (S.Str (acc <> e)) tail
+    combine (List.Cons (S.Str acc) tail) S.Space =
+        List.Cons (S.Str (acc <> " ")) tail
+    combine acc e = List.Cons e acc
+  in
+    foldl combine List.Nil
 
 toArray :: forall a. List a -> Array a
 toArray = List.toUnfoldable
